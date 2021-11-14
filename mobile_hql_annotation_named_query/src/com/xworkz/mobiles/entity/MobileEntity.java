5f125c9c91f10fbcf78a0d2562d6c9d41f90871a
Rakesh <you@example.com>
2021-11-15 00:47:24 +0530

    Hibernate with HQL,@namedquery

---
+++ mobile_hql_annotation_named_query/src/com/xworkz/mobiles/entity/MobileEntity.java
@@ -0,0 +1,143 @@
+package com.xworkz.mobiles.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.NamedQuery;
+//import org.hibernate.id.IdentityGenerator;
+import org.hibernate.id.IncrementGenerator;
+
+@Table(name = "details_of_mobiles2")
+@Entity
+@NamedQuery(name = "MobileEntity.getMobileBrandByID", query = "select mobileBrand from MobileEntity where mobileID=:MobileID")
+@NamedQuery(name = "MobileEntity.getMobileEntityByID", query = "from MobileEntity where mobileID=:MobileID")
+@NamedQuery(name = "MobileEntity.updateMobilePriceByID", query = "update MobileEntity set mobilePrice=:MobilePrice where mobileID=:MobileID")
+@NamedQuery(name = "MobileEntity.deleteMobileEntityByID", query = "delete from MobileEntity where mobileID=:MobileID")
+@NamedQuery(name = "MobileEntity.updateMobilePriceByMobileBrand", query = "update MobileEntity set mobilePrice=:MobilePrice where mobileBrand=:MobileBrand")
+@NamedQuery(name = "MobileEntity.getMobileEntityByMobileBrand", query = "from MobileEntity where mobileBrand=:MobileBrand")
+@NamedQuery(name = "MobileEntity.deleteMobileEntityByMobileBrand", query = "delete from MobileEntity where mobileBrand=:MobileBrand")
+public class MobileEntity implements java.io.Serializable {
+
+	@Id
+	@Column(name = "MOBILE_ID")
+
+	@GeneratedValue(generator = "abc")
+	@GenericGenerator(name = "abc", strategy = "increment")
+
+	private int mobileID;
+
+	@Column(name = "MOBILE_BRAND")
+	private String mobileBrand;
+
+	@Column(name = "MOBILE_PRICE")
+	private double mobilePrice;
+
+	@Column(name = "RAM")
+	private String ram;
+
+	@Column(name = "COLOR")
+	private String color;
+
+	@Column(name = "CAMERA_SIZE ")
+	private int cameraSize;
+
+	@Column(name = "IS_FINGER_PRINT_SUPPORTED")
+	private boolean isFingerPrintSupported;
+
+	@Column(name = "OS_TYPE")
+	private String osType;
+
+	public MobileEntity() {
+		super();
+		IncrementGenerator generator = new IncrementGenerator();
+
+	}
+
+	public MobileEntity(String mobileBrand, double mobilePrice, String ram, String color, int cameraSize,
+			boolean isFingerPrintSupported, String osType) {
+		super();
+		this.mobileBrand = mobileBrand;
+		this.mobilePrice = mobilePrice;
+		this.ram = ram;
+		this.color = color;
+		this.cameraSize = cameraSize;
+		this.isFingerPrintSupported = isFingerPrintSupported;
+		this.osType = osType;
+	}
+
+	public int getMobileID() {
+		return mobileID;
+	}
+
+	public void setMobileID(int mobileID) {
+		this.mobileID = mobileID;
+	}
+
+	public String getMobileBrand() {
+		return mobileBrand;
+	}
+
+	public void setMobileBrand(String mobileBrand) {
+		this.mobileBrand = mobileBrand;
+	}
+
+	public double getMobilePrice() {
+		return mobilePrice;
+	}
+
+	public void setMobilePrice(double mobilePrice) {
+		this.mobilePrice = mobilePrice;
+	}
+
+	public String getRam() {
+		return ram;
+	}
+
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getCameraSize() {
+		return cameraSize;
+	}
+
+	public void setCameraSize(int cameraSize) {
+		this.cameraSize = cameraSize;
+	}
+
+	public boolean isFingerPrintSupported() {
+		return isFingerPrintSupported;
+	}
+
+	public void setFingerPrintSupported(boolean isFingerPrintSupported) {
+		this.isFingerPrintSupported = isFingerPrintSupported;
+	}
+
+	public String getOsType() {
+		return osType;
+	}
+
+	public void setOsType(String osType) {
+		this.osType = osType;
+	}
+
+	@Override
+	public String toString() {
+		return "MobileEntity [mobileID=" + mobileID + ", mobileBrand=" + mobileBrand + ", mobilePrice=" + mobilePrice
+				+ ", rom=" + ram + ", color=" + color + ", cameraSize=" + cameraSize + ", isFingerPrintSupported="
+				+ isFingerPrintSupported + ", osType=" + osType + "]";
+	}
+
+}
